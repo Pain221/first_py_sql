eb76f48435a75f204e5fd52f1714d1863f94f0cd
Pain221 <131016895+Pain221@users.noreply.github.com>
2023-04-17 22:30:08 +0500

    Add files via upload

---
+++ pet.py
@@ -0,0 +1,12 @@
+HVS_device=''
+GVS_device=''
+EE_device=''
+def Request():  
+    number_of_residents=input('введите количество проживающих в помещении')
+    HVS_device=input('есть ли у вас прибор учета ХВС?(введите ДА или НЕТ)')
+    GVS_device=input('есть ли у вас прибор учета ГВС?(введите ДА или НЕТ)')
+    EE_device=input('есть ли у вас прибор учета ЭЭ?(введите ДА или НЕТ)')
+
+#def Collecting_data():
+#if HVS_device=='Да':
+#подумать про базу данных
