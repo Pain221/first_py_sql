b9421543434d4efd46bfa55311ac2e1f178827d7
Pain221 <131016895+Pain221@users.noreply.github.com>
2023-04-18 10:15:21 +0500

    Update pet.py

--- pet.py
+++ pet.py
@@ -1,12 +1,33 @@
-HVS_device=''
-GVS_device=''
-EE_device=''
-def Request():  
+import psycopg2
+import datetime
+
+conn=psycopg2.connect("dbname=komunalka user=postgres password=123")
+cursor=conn.cursor()
+
+def Request_data():  
+    global HVS_device, GVS_device, EE_device, number_LS, request_date
+    number_LS=int(input('Введите ваш номер лицевого счета'))
     number_of_residents=input('введите количество проживающих в помещении')
+    request_date=datetime.date.today()
     HVS_device=input('есть ли у вас прибор учета ХВС?(введите ДА или НЕТ)')
     GVS_device=input('есть ли у вас прибор учета ГВС?(введите ДА или НЕТ)')
     EE_device=input('есть ли у вас прибор учета ЭЭ?(введите ДА или НЕТ)')
+    return number_LS,request_date
+def Collecting_data():
+    try:
+        if HVS_device=='ДА' and GVS_device=='ДА' and EE_device=='ДА':
+            indications_HVS=int(input('введите показания ХВС'))
+            indications_GVS=int(input('введите показания ГВС'))
+            indications_EE=int(input('введите показания ЭЭ'))
+            cursor.execute("""INSERT INTO Customers(number_ls, indications_date, indications_hvs, indications_gvs, indications_ee) 
+            VALUES (%s, %s, %s, %s, %s);""", (number_LS, request_date, indications_HVS, indications_GVS, indications_EE))
+            conn.commit()
+            print('Показания внесены!')
+    except Exception:
+        conn.rollback()
+        sys.exc_info()
+        print('ошибка!')
+        
 
-#def Collecting_data():
-#if HVS_device=='Да':
-#подумать про базу данных
+request_list=Request_data()
+Collecting_data()
