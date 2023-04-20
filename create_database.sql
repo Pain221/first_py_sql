7b6a4bbc071c60bc6c1d5bb8af9602d7a605126e
Pain221 <131016895+Pain221@users.noreply.github.com>
2023-04-20 10:53:01 +0500

    Delete create_database.sql

--- create_database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Customers 
-(
-	number_LS INTEGER NOT NULL PRIMARY KEY,
-	indications_date DATE NOT NULL,
-	indications_HVS NUMERIC,
-	indications_GVS NUMERIC,
-	indications_EE NUMERIC
-);
