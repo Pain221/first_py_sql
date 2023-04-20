882678590d94bf3b207e40ec7fda4dcbca521b51
Pain221 <131016895+Pain221@users.noreply.github.com>
2023-04-20 10:52:07 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import prototype as pt
+import calculations as cl
+import connections as cn
+
+#коннект и курсор
+cn.connection_open()
+
+#сбор дпоказаний и занесение в базу
+pt.activation()
+
+#выполнение вычислений
+cl.activate_calculations()
+
+#закрытие коннекта и курсора
+cn.connection_close()
+
