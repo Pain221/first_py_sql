586543a8b8edd03010408169f58e71b35e749808
Pain221 <131016895+Pain221@users.noreply.github.com>
2023-04-21 10:28:52 +0500

    Add files via upload

--- calculations.py
+++ calculations.py
@@ -16,38 +16,57 @@ def database_record_accrual(number_LS,request_date):
     cursor.execute("""INSERT INTO Accruals (number_ls, accrual_date) VALUES (%s, %s);""",(number_LS, request_date))
 
 #определяет какие функции выполнять
-def Execute_calculations(HVS_device, indications_HVS, GVS_device,indications_GVS, number_of_residents, number_LS,request_date):
+def Execute_calculations(HVS_device, GVS_device, EE_device, number_of_residents, number_LS,request_date):
     if HVS_device == 'НЕТ':
         HVS_without_device(number_of_residents, number_LS, request_date)
     if HVS_device == 'ДА':
-        HVS_with_device(number_LS, request_date, indications_HVS)
+        HVS_with_device(number_LS, request_date)
     if GVS_device=='НЕТ':
         GVS_heat_carrier_without_device(number_LS, request_date, number_of_residents)
     if GVS_device == 'ДА':
-        GVS_heat_carrier_with_device(number_LS, request_date,indications_GVS)
-    GVS_thermal_energy(number_LS, request_date, number_of_residents, volume_of_consumption_GVS_heat_carrier)
+        GVS_heat_carrier_with_device(number_LS, request_date)
+    GVS_thermal_energy(number_LS, request_date,  volume_of_consumption_GVS_heat_carrier)
+    Total_GVS(number_LS, request_date, accrual_GVS_thermal_energy,accrual_GVS_heat_carrier)
+    if EE_device == 'НЕТ':
+        EE_without_device(number_LS, request_date, number_of_residents)
+    if EE_device == 'ДА':
+        EE_with_device(number_LS, request_date)
+    Total_accrual(number_LS,request_date)
 
 #начисления за ХВС без счётчика
 def HVS_without_device(number_of_residents, number_LS, request_date):
+    global accrual_HVS
     volume_of_consumption=number_of_residents*standard_frame['standard'][0]
-    accrual=volume_of_consumption*standard_frame['tariff(rub/unit_of_measurement)'][0] #accrual - начисление
+    accrual_HVS=volume_of_consumption*standard_frame['tariff(rub/unit_of_measurement)'][0] #accrual - начисление
     try:
-        cursor.execute("""UPDATE Accruals SET accrual_hvs=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual, number_LS, request_date))
+        cursor.execute("""UPDATE Accruals SET accrual_hvs=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual_HVS, number_LS, request_date))
         conn.commit()
-        print('Начисления за ХВС вычислены:', accrual, 'рублей!')
+        print('Начисления за ХВС вычислены:', ("{:8.2f}".format(accrual_HVS)), 'рублей!')
     except Exception as e:
         conn.rollback()
         print('ошибка!')
         print(e)
 
 #начисления за ХВС со счётчиком
-def HVS_with_device(number_LS, request_date, indications_HVS):
-    volume_of_consumption=indications_HVS
-    accrual=volume_of_consumption*standard_frame['tariff(rub/unit_of_measurement)'][0] #accrual - начисление
+def HVS_with_device(number_LS, request_date):
+    from prototype import indications_HVS
+
+    #вытаскивание прошлых данных
     try:
-        cursor.execute("""UPDATE Accruals SET accrual_hvs=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual, number_LS, request_date))
+        cursor.execute("""SELECT * FROM Customers WHERE number_ls='%s' ORDER BY indications_date DESC LIMIT 1 OFFSET 1;""",(number_LS,)) 
+        datas=cursor.fetchall()
+        last_record_DF=pd.DataFrame(datas)
+        last_HVS_indications=int(last_record_DF[2][0])
+    except Exception as e:
+        last_HVS_indications=0
+
+    global accrual_HVS
+    volume_of_consumption=indications_HVS-last_HVS_indications
+    accrual_HVS=volume_of_consumption*standard_frame['tariff(rub/unit_of_measurement)'][0] #accrual - начисление
+    try:
+        cursor.execute("""UPDATE Accruals SET accrual_hvs=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual_HVS, number_LS, request_date))
         conn.commit()
-        print('Начисления за ХВС вычислены:', accrual, 'рублей!')
+        print('Начисления за ХВС вычислены:', ("{:8.2f}".format(accrual_HVS)), 'рублей!')
     except Exception as e:
         conn.rollback()
         print('ошибка!')
@@ -55,27 +74,39 @@ def HVS_with_device(number_LS, request_date, indications_HVS):
 
 #начисления за ГВС Теплоноситель без счетчика
 def GVS_heat_carrier_without_device(number_LS, request_date, number_of_residents):
-    global volume_of_consumption_GVS_heat_carrier
+    global volume_of_consumption_GVS_heat_carrier, accrual_GVS_heat_carrier
     volume_of_consumption_GVS_heat_carrier =number_of_residents*standard_frame['standard'][1] #accrual - начисление
-    accrual=volume_of_consumption_GVS_heat_carrier*standard_frame['tariff(rub/unit_of_measurement)'][1]
+    accrual_GVS_heat_carrier=volume_of_consumption_GVS_heat_carrier*standard_frame['tariff(rub/unit_of_measurement)'][1]
     try:
-        cursor.execute("""UPDATE Accruals SET accrual_gvs_heat_carrier=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual, number_LS, request_date))
+        cursor.execute("""UPDATE Accruals SET accrual_gvs_heat_carrier=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual_GVS_heat_carrier, number_LS, request_date))
         conn.commit()
-        print('Начисления за ГВС Теплоноситель вычислены:', accrual, 'рублей!')
+        print('Начисления за ГВС Теплоноситель вычислены:', ("{:8.2f}".format(accrual_GVS_heat_carrier)), 'рублей!')
     except Exception as e:
         conn.rollback()
         print('ошибка!')
         print(e) 
 
 #начисления за ГВС Теплоноситель со счётчиком
-def GVS_heat_carrier_with_device(number_LS, request_date,indications_GVS):
-    global volume_of_consumption_GVS_heat_carrier #
-    volume_of_consumption_GVS_heat_carrier = indications_GVS
-    accrual=volume_of_consumption_GVS_heat_carrier*standard_frame['tariff(rub/unit_of_measurement)'][1]
+def GVS_heat_carrier_with_device(number_LS, request_date):
+    #вытаскивание прошлых данных
     try:
-        cursor.execute("""UPDATE Accruals SET accrual_gvs_heat_carrier=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual, number_LS, request_date))
+        cursor.execute("""SELECT * FROM Customers WHERE number_ls='%s' ORDER BY indications_date DESC LIMIT 1 OFFSET 1;""",(number_LS,)) 
+        datas=cursor.fetchall()
+        last_record_DF=pd.DataFrame(datas)
+        last_GVS_indications=int(last_record_DF[3][0])
+    except Exception as e:
+        last_GVS_indications=0
+
+    global accrual_GVS_heat_carrier
+    from prototype import indications_GVS
+    global volume_of_consumption_GVS_heat_carrier
+
+    volume_of_consumption_GVS_heat_carrier = indications_GVS - last_GVS_indications
+    accrual_GVS_heat_carrier=volume_of_consumption_GVS_heat_carrier*standard_frame['tariff(rub/unit_of_measurement)'][1]
+    try:
+        cursor.execute("""UPDATE Accruals SET accrual_gvs_heat_carrier=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual_GVS_heat_carrier, number_LS, request_date))
         conn.commit()
-        print('Начисления за ГВС Теплоноситель вычислены:', accrual, 'рублей!')
+        print('Начисления за ГВС Теплоноситель вычислены:', ("{:8.2f}".format(accrual_GVS_heat_carrier)), 'рублей!')
     except Exception as e:
         conn.rollback()
         print('ошибка!')
@@ -83,26 +114,91 @@ def GVS_heat_carrier_with_device(number_LS, request_date,indications_GVS):
 
 
 #начисления за ГВС Тепловая энергия
-def GVS_thermal_energy(number_LS, request_date, number_of_residents, volume_of_consumption_GVS_heat_carrier):
+def GVS_thermal_energy(number_LS, request_date, volume_of_consumption_GVS_heat_carrier):
+    global accrual_GVS_thermal_energy
     volume_of_consumption=volume_of_consumption_GVS_heat_carrier*standard_frame['standard'][6]
-    accrual=volume_of_consumption*standard_frame['tariff(rub/unit_of_measurement)'][6] #accrual - начисление
+    accrual_GVS_thermal_energy=volume_of_consumption*standard_frame['tariff(rub/unit_of_measurement)'][6] #accrual - начисление
     try:
-        cursor.execute("""UPDATE Accruals SET accrual_gvs_thermal_energy=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual, number_LS, request_date))
+        cursor.execute("""UPDATE Accruals SET accrual_gvs_thermal_energy=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual_GVS_thermal_energy, number_LS, request_date))
         conn.commit()
-        print('Начисления за ГВС Тепловая энергия вычислены:', accrual, 'рублей!')
+        print('Начисления за ГВС Тепловая энергия вычислены:', ("{:8.2f}".format(accrual_GVS_thermal_energy)), 'рублей!')
     except Exception as e:
         conn.rollback()
         print('ошибка!')
         print(e) 
 
+#итоговые начисления за ГВС
+def Total_GVS(number_LS, request_date, accrual_GVS_thermal_energy,accrual_GVS_heat_carrier):
+    global accrual_GVS
+    accrual_GVS=accrual_GVS_thermal_energy+accrual_GVS_heat_carrier
+    try:
+        cursor.execute("""UPDATE Accruals SET accrual_total_gvs=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual_GVS, number_LS, request_date))
+        conn.commit()
+        print('Итоговые начисления за ГВС:', ("{:8.2f}".format(accrual_GVS)), 'рублей!')
+    except Exception as e:
+        conn.rollback()
+        print('ошибка!')
+        print(e)
+
+
 #начисления за Электроэнергию без счетчика
 def EE_without_device(number_LS, request_date, number_of_residents):
+    global accrual_EE
     volume_of_consumption=number_of_residents*standard_frame['standard'][2]
-    accrual=volume_of_consumption*standard_frame['tariff(rub/unit_of_measurement)'][2] #accrual - начисление
-    #добавить внос начисления в базу 
+    accrual_EE=volume_of_consumption*standard_frame['tariff(rub/unit_of_measurement)'][2] #accrual - начисление
+    try:
+        cursor.execute("""UPDATE Accruals SET accrual_ee=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual_EE, number_LS, request_date))
+        conn.commit()
+        print('Начисления за Электроэнергию вычислены:', ("{:8.2f}".format(accrual_EE)), 'рублей!')
+    except Exception as e:
+        conn.rollback()
+        print('ошибка!')
+        print(e) 
+
+#начисления за Электроэнергию со счётчиком
+def EE_with_device(number_LS, request_date):
+    from prototype import indications_EE_daytime, indications_EE_night
+
+    #вытаскивание прошлых данных
+    try:
+        cursor.execute("""SELECT * FROM Customers WHERE number_ls='%s' ORDER BY indications_date DESC LIMIT 1 OFFSET 1;""",(number_LS,)) 
+        datas=cursor.fetchall()
+        last_record_DF=pd.DataFrame(datas)
+        last_indications_EE_daytime=int(last_record_DF[4][0])
+        last_indications_EE_night=int(last_record_DF[6][0])
+    except Exception as e:
+        last_indications_EE_daytime=0
+        last_indications_EE_night=0
+
+    global accrual_EE
+    volume_of_consumption=(indications_EE_daytime-last_indications_EE_daytime)+(indications_EE_night-last_indications_EE_night)
+    accrual_daytime=volume_of_consumption*standard_frame['tariff(rub/unit_of_measurement)'][3]
+    accrual_night=indications_EE_night*standard_frame['tariff(rub/unit_of_measurement)'][4]
+    accrual_EE=accrual_daytime+accrual_night
+
+    try:
+        cursor.execute("""UPDATE Accruals SET accrual_ee=%s WHERE number_LS=%s and accrual_date=%s;""",(accrual_EE, number_LS, request_date))
+        conn.commit()
+        print('Начисления за Электроэнергию вычислены:', ("{:8.2f}".format(accrual_EE)), 'рублей!')
+    except Exception as e:
+        conn.rollback()
+        print('ошибка!')
+        print(e) 
+
+#Общий итог по начислениям
+def Total_accrual(number_LS,request_date):
+    total_accrual=accrual_HVS+accrual_GVS+accrual_EE
+    try:
+        cursor.execute("""UPDATE Accruals SET total_accrual=%s WHERE number_LS=%s and accrual_date=%s;""",(total_accrual, number_LS, request_date))
+        conn.commit()
+        print('Общий итог по начислениям: ', ("{:8.2f}".format(total_accrual)), 'рублей!')
+    except Exception as e:
+        conn.rollback()
+        print('ошибка!')
+        print(e)
 
 #главная функция для main
 def activate_calculations():
-    from prototype import number_of_residents, HVS_device, number_LS, request_date, GVS_device, indications_HVS, indications_GVS
+    from prototype import number_of_residents, HVS_device, number_LS, request_date, GVS_device, EE_device
     database_record_accrual(number_LS,request_date)
-    Execute_calculations(HVS_device, indications_HVS, GVS_device, indications_GVS, number_of_residents, number_LS,request_date)
+    Execute_calculations(HVS_device,  GVS_device,  EE_device, number_of_residents, number_LS, request_date)
