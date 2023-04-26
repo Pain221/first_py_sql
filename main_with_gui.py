61a5c0d180f937123983235203bbe74e1e9f0a30
Pain221 <131016895+Pain221@users.noreply.github.com>
2023-04-26 10:50:07 +0500

    Add files via upload

--- main_with_gui.py
+++ main_with_gui.py
@@ -1,12 +1,13 @@
 import connections as cn
 import gui_tkinter as gui
-import calculations_with_gui as cl
+
 
 #коннект и курсор
 cn.connection_open()
 
-#запуск интерфейса, сбор и внесение показаний в базу
-gui.first_page()
+#вся работа
+gui.manage()
 
+#закрытие коннекта и курсора
 cn.connection_close()
 
