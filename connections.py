0efac86199a481f70a1ab95cea2ecc88a5cb153d
Pain221 <131016895+Pain221@users.noreply.github.com>
2023-04-29 22:08:51 +0500

    Update connections.py

--- connections.py
+++ connections.py
@@ -2,7 +2,7 @@ import psycopg2 as pc2
 
 def connection_open():
     global conn,cursor
-    conn=pc2.connect("dbname=komunalka user=postgres password=123 port=5432")
+    conn=pc2.connect("dbname=*** user=*** password=*** port=***")
     cursor=conn.cursor()
 
 def connection_close():
